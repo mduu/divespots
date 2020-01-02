f4e4cb621f9d863fd405fc65d4635ed647c78e3e
Marc Dürst <marc@marcduerst.com>
2020-01-02 15:26:08 +0100

    F Add WaterOverviewContentView

---
+++ src/MobileApps/DiveSpots/DiveSpots/Views/WaterOverviewContentView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace DiveSpots.Views
+{
+    public partial class WaterOverviewContentView : ContentView
+    {
+        public WaterOverviewContentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
